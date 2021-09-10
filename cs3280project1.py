1c856d915199bb6564e622fe5e99781a80f09c28
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-09-10 14:40:02 -0400

    Got card issuers to show

--- cs3280project1.py
+++ cs3280project1.py
@@ -9,34 +9,18 @@ import utils
 __author__ = 'Cody Vollrath'
 __version__ = 'Fall 2021'
 __pylint__ = '1.8.3.'
-
-
-def get_resource():
-    """
-    Reads from the path to file as input from the command line args
-    Returns: content of the file
-    """
-    content = None
-    try:
-        with open(sys.argv[1]) as input_file:
-            content = input_file.read()
-        input_file.close()
-        return content
-    except IOError as error:
-        print('Error:',  error)
-
+ 
 def main():
     """
     Run function for the project 1 assignment
     Args: - credit_card_number the credit card number entered by the user
     """
-    content = get_resource();
+    content = utils.get_file_data(sys.argv[1]).replace('\n','*')
     credit_card_number = input("Please enter a credit card number: ")
     if utils.verify_credit_card(credit_card_number):
-        print("Valid Credit Card!")
-    else:
-        print("Invalid Credit Card!")
-
+        data_map = utils.parse_csv(content)
+        credit_card_type = utils.determine_card_issuer(credit_card_number, data_map)
+        print(credit_card_type)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -21,3 +21,61 @@ def verify_credit_card(credit_card_number):
     if not credit_card_regex.match(credit_card_number):
         return False
     return True
+
+def get_file_data(filename):
+    """
+    Gets the file content from the file path that is passed into the function
+    Returns: content of file if opened
+    Raises: IOError if the file can not be opened and displays the error message
+    """
+    content = None
+    try:
+        with open(filename) as input_file:
+            content = input_file.read()
+        input_file.close()
+        return content
+    except IOError as error:
+        print('Error:',  error)
+
+def parse_csv(csv_str):
+    """
+    Takes the CSV data from the passed in string and converts it into a dict
+    Args: csv_str - the csv string to be parsed
+    returns the dict of the csv data
+    """
+    data_map = {}
+    line_split_csv = csv_str.split('*')
+    for element in line_split_csv:
+        fields = element.split(';')
+        data_map[fields[0]] = fields[1:]
+    data_map.pop('')
+    return data_map
+
+def determine_card_issuer(credit_card_number, data_map):
+    card_issuer = None
+    for key in data_map:
+        data_detail = data_map[key]
+        lengths = data_detail[0]
+        prefixes = data_detail[1]
+        if __is_length_of(credit_card_number, lengths) and __prefix_matches(credit_card_number, prefixes):
+            card_issuer = key
+            return card_issuer
+    return card_issuer
+
+def __is_length_of(credit_card_number, lengths):
+    lengths = lengths.split(',')
+    for length in lengths:
+        if int(length) == len(credit_card_number):
+            return True
+    return False
+def __prefix_matches(credit_card_number, prefixes):
+    prefixes = prefixes.split(',')
+    for prefix in prefixes:
+        if '-' in prefix:
+            ranges = prefix.split('-')
+            if int(credit_card_number[0:len(ranges[0]) + 1]) >= int(ranges[0]) and int(credit_card_number[0:len(ranges[1]) + 1]) <= int(ranges[1]):
+                return True
+        if credit_card_number.startswith(prefix):
+            return True
+    return False
+
